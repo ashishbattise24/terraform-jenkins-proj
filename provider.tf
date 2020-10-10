cbfd0c9727c9c6081d1595b5c71cf67d66bc2fe2
ashishbattise24 <38582011+ashishbattise24@users.noreply.github.com>
2020-10-10 11:39:27 +0530

    provider_update1
    provider_update1

--- provider.tf
+++ provider.tf
@@ -1,15 +1,5 @@
 provider "aws" {
- access_key = "AKIAXFPDNIDGYHPM2I6T"
- secret_key = "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"
+
  region = "ap-southeast-1"
 }
 
-resource "aws_vpc" "myvpc_dev"{
-cidr_block = "${var.vpc_cidr_dev}"
-
-}
-resource "aws_vpc" "myvpc_prod"{
-
- cidr_block = "${var.vpc_cidr_prod}"
-
-}
